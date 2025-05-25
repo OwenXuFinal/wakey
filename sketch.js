d698cd4d6dbcc3d9e22b5ad2cf430e9f1f927bc6
OwenXuFinal <owenxuni@gmail.com>
2025-05-25 18:04:05 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let roll = 0;
 function setup() {
   createCanvas(400, 400);
 
-  serial = new p5.SerialPort();
+  serial = new p5.Serial();
 
   let btn = createButton("Connect");
   btn.position(10, 10);
